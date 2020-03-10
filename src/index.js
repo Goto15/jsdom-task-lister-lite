5f4e0d704a777a742e9b74ecf60fbf46b7d7aed3
GoTo15 <github email address>
2020-03-10 12:06:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+function addNewListItem(value, listID) {
+  const newNode = document.getElementById(listID);
+  const newListItem = document.createElement('li');
+
+  newListItem.appendChild(document.createTextNode(value));
+  newNode.appendChild(newListItem);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .querySelector('#create-task-form')
+    .addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const taskContent = document.getElementById('new-task-description').value;
+      addNewListItem(taskContent, 'tasks');
+    });
 });
